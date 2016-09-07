382e2242b8a7a37c1b3cf9379de0965e82942e56
Lacey Knaff <lacey.knaff@gmail.com>
2016-09-07 16:29:36 -0600

    wrote JSON value to convert idea div to string on click

--- scripts.js
+++ scripts.js
@@ -8,14 +8,36 @@ title and body input fields are cleared
 page should not reload when input fields are cleared*/
 
 
+//function to stringify
+
+
 function addIdeaToPage(ideaTitle, ideaBody) {
-  $('.ideaList').prepend ('<div id=' + Date.now() + ' class="container"><h2 class="ideaTitle">' + ideaTitle + '</h2><button>delete</button><p class ="ideaBody">' + ideaBody + '</p><button>uparrow</button><button>downarrow</button><p class="ideaQuality">swill</p></div>');
+  var ideaVariable = ('<div id=' + Date.now() + ' class="container"><h2 class="ideaTitle">' + ideaTitle + '</h2><button>delete</button><p class ="ideaBody">' + ideaBody + '</p><button>uparrow</button><button>downarrow</button><p class="ideaQuality">swill</p></div>');
+
+  $('.ideaList').prepend (ideaVariable);
+
+  JSON.stringify(ideaVariable);
+  // var stringifiedIdea = JSON.stringify(ideaVariable);
+  localStorage.setItem('ideaKey', ideaVariable);
 };
 
+// $("#idea-list").load(function() {
+//   var object = localStorage.getItem('ideaKey')
+//   JSON.parse(object);
+//   localStorage.getItem('ideaKey');
+});
+
+// JSON.parse(localStorage.getItem('ideaKey'));
+
+function clearInputFields(){
+  $('#title-input').val('');
+  $('#body-input').val('');
+}
 
 //event lister save button
 $('#save-btn').on('click', function() {
   var ideaTitle = $('#title-input').val();
   var ideaBody = $('#body-input').val();
   addIdeaToPage(ideaTitle, ideaBody);
+  clearInputFields();
 });
